2e3fb03a8d5e34f37d24fb3e2a3c12d51c8c2fd7
radhikagupta-31 <radgupta31@gmail.com>
2026-09-08 21:56:14 +0530

    Time: 10 ms (14.26%), Space: 23.8 MB (10.39%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,10 +1,34 @@
 class Solution {
 public:
+//xor
     int singleNumber(vector<int>& nums) {
-    int xor1 =0;
-    for (int i = 0 ; i< nums.size(); i++){
-        xor1 = (xor1 ^ nums[i]);
-      }  
-      return xor1;
+    // int xor1 =0;
+    // for (int i = 0 ; i< nums.size(); i++){
+    //     xor1 = (xor1 ^ nums[i]);
+    //   }  
+    //   return xor1;
+// Hashing
+    // int maxi = nums[0];
+    // for (int i =0 ;i<nums.size(); i++){
+    //     maxi = max (maxi, nums[i]);
+    // }
+    // vector<int> hash(maxi);
+    // hash[maxi]= {0};
+    // for (int i=0;i<maxi; i++){
+    //     hash[nums[i]]++;
+    // }for(int i =0; i<nums.size(); i++){
+    // if (hash[nums[i]] == 1)
+    // return nums[i];
+    // }
+    // return 0;
+    unordered_map <int , int> mp;
+    for (int i=0; i< nums.size(); i++){
+        mp[nums[i]]++;
+    }
+    for (auto i : mp){
+        if (i.second ==1)
+        return i.first;
+    }
+    return 0;
     }
 };
