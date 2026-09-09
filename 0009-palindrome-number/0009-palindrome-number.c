361dca78293f7fc6a5e98a6f401e47b2e8073938
radhikagupta-31 <radgupta31@gmail.com>
2026-09-09 23:33:16 +0530

    Time: 0 ms (100%), Space: 8.8 MB (63.07%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -0,0 +1,15 @@
+bool isPalindrome(int x) {
+    int temp = x;
+    long ans =0;
+    if ( x< 0)
+    return false;
+    while (x!=0){
+        int rem = x%10;
+        ans = ans*10 + rem;
+       x= x/10;
+    }
+    if (ans == temp)
+    return true;
+    else 
+    return false;
+}
