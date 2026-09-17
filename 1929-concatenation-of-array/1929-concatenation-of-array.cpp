8a68e2c0f7f77d53dac21bb1a1d53613fa730658
radhikagupta-31 <radgupta31@gmail.com>
2026-09-18 00:04:43 +0530

    Time: 0 ms (100%), Space: 17.2 MB (16.49%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -3,11 +3,9 @@ public:
     vector<int> getConcatenation(vector<int>& nums) {
        int n = nums.size();
        vector<int>ans;
-       for (int i = 0 ; i< n; i++){
-            ans.push_back(nums[i]);
-       }
-       for(int i=0;i<n;i++){
-        ans.push_back(nums[i]);
+       for (int i = 0 ; i< 2; i++){
+        for (int j = 0; j<n ; j++)
+            ans.push_back(nums[j]);
        }
        return ans;
     }
